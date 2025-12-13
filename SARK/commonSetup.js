da28ab2c6b31a5347fd11aa5921ac3ac7bb5ea45
AnInsaneJimJam <anand47bansal@gmail.com>
2025-12-14 01:46:08 +0530

    Common setup complete

--- SARK/commonSetup.js
+++ SARK/commonSetup.js
@@ -4,22 +4,23 @@ import {P, CIRCUIT} from "./parameters.js";
 import {getCircuitDetails,getSubPolynomailPoints} from "../utils/circuit.js";
 
 const circuit = getCircuitDetails(CIRCUIT);
+const ff = new FiniteField(P);
 
 function getGPolynomial(){
     const points = circuit.map(elem => [BigInt( elem[0]),0n])
-    return Polynomial.lagrange(points)
+    return Polynomial.lagrange(points,ff)
 }
 
 function getSubPolynomials(pos){
     let result = []
     for(let i = 1; i <= circuit.length+1;i++){
         let subpoly = getSubPolynomailPoints(circuit,pos,i);
-        result.push(Polynomial.lagrange(subpoly));
+        result.push(Polynomial.lagrange(subpoly,ff));
     }
     return result
 }
 
-let polynomialG = getGPolynomial();
-let polynomialsLi = getSubPolynomials(1);
-let polynomialsRi = getSubPolynomials(2);
-let polynomialsOi = getSubPolynomials(3);
+export const polynomialG = getGPolynomial();
+export const polynomialsLi = getSubPolynomials(1);
+export const polynomialsRi = getSubPolynomials(2);
+export const polynomialsOi = getSubPolynomials(3);
