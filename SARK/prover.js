ee4791f4f438b5560bcf6cec9b87d337e1ee56b4
AnInsaneJimJam <anand47bansal@gmail.com>
2025-12-17 16:28:09 +0530

    Added correct knowledge of exponent values and circuit eval

--- SARK/prover.js
+++ SARK/prover.js
@@ -1,42 +1,74 @@
-import FiniteField from "../utils/finitefield";
-import Polynomial from "../utils/polynomial";
-import { polynomialG, polynomialsLi, polynomialsOi, polynomialsRi } from "./commonSetup"
-import { P } from "./parameters";
+import FiniteField from "../utils/finitefield.js";
+import Polynomial from "../utils/polynomial.js";
+import { polynomialG, polynomialsLi, polynomialsOi, polynomialsRi } from "./commonSetup.js"
+import { CIRCUIT, I, P } from "./parameters.js";
+import { getCircuitDetails } from "../utils/circuit.js";
 
-/* TODO: Reading logic of the communicated values provided by Veronica
-    input: I
-    encrptedLmids,encryptedAlphaLmids ....
-*/
+import fs from 'fs';
+import { bigIntParse } from '../utils/serialization.js';
 
-ff = new FiniteField(P)
+const provingKeyPath = new URL('./proving_key.json', import.meta.url).pathname;
+console.log(`Loading proving key from ${provingKeyPath}...`);
+const provingKey = bigIntParse(fs.readFileSync(provingKeyPath, 'utf8'));
+
+const {
+    encryptedLs,
+    encryptedRs,
+    encryptedOs,
+    encryptedAlphaLs,
+    encryptedAlphaRs,
+    encryptedAlphaOs,
+    encryptedSubPolynomialSum,
+    PowersOfG
+} = provingKey;
+
+const ff = new FiniteField(P)
+
+
 
 function evalcircuit(input) {
-    // TODO: Implement circuit
+    const circuit = getCircuitDetails(BigInt(CIRCUIT));
+    const wires = { 1: input };
+
+    for (let i = 0; i < circuit.length; i++) {
+        const [gateNo, left, right, out] = circuit[i];
+        const leftVal = wires[left];
+        const rightVal = wires[right];
+        wires[out] = ff.mul(leftVal, rightVal);
+    }
+
+    const result = [];
+    const numWires = circuit.length + 1;
+    for(let i = 1; i <= numWires; i++) {
+        result.push(wires[i]);
+    }
+    return result
 }
 
-const Ci = evalcircuit(I)
+const Ci = evalcircuit(I);
 
+// make it more readable later
 const noOfGates = polynomialsLi.length
 
 function getSubPoly(pos) {
     let result;
     switch (pos) {
         case 1:
-            result = new Polynomial([scalar], ff).mul(polynomialsLi[0])
+            result = new Polynomial([Ci[0]], ff).mul(polynomialsLi[0])
             for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsLi[i]));
+                result = result.add(new Polynomial([Ci[i]], ff).mul(polynomialsLi[i]));
             }
             break;
         case 2:
-            result = new Polynomial([scalar], ff).mul(polynomialsRi[0])
+            result = new Polynomial([Ci[0]], ff).mul(polynomialsRi[0])
             for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsRi[i]));
+                result = result.add(new Polynomial([Ci[i]], ff).mul(polynomialsRi[i]));
             }
             break;
         case 3:
-            result = new Polynomial([scalar], ff).mul(polynomialsOi[0])
+            result = new Polynomial([Ci[0]], ff).mul(polynomialsOi[0])
             for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsOi[i]));
+                result = result.add(new Polynomial([Ci[i]], ff).mul(polynomialsOi[i]));
             }
             break;
     }
@@ -48,41 +80,27 @@ const polynomialW = getSubPoly(1).mul(getSubPoly(2)).sub(getSubPoly(3));
 
 const polynomialH = polynomialW.div(polynomialG);
 
+console.log(polynomialH);
 
-// TODO: Correct later
-function getEncryptedInterSubPoly(pos) {
-    let result;
-    switch (pos) {
-        case 1:
-            result = new Polynomial([scalar], ff).mul(polynomialsLi[0])
-            for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsLi[i]));
-            }
-            break;
-        case 2:
-            result = new Polynomial([scalar], ff).mul(polynomialsRi[0])
-            for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsRi[i]));
-            }
-            break;
-        case 3:
-            result = new Polynomial([scalar], ff).mul(polynomialsOi[0])
-            for (let i = 1n; i < noOfGates; i++) {
-                result = result.add(new Polynomial([scalar], ff).mul(polynomialsOi[i]));
-            }
-            break;
+function getEncryptedInterSubPoly(arr) {
+    let result =1n;
+    for (let i = 1; i < arr.length-1; i++) {
+        result = ff.mul(result,ff.pow(arr[i],Ci[i]));
     }
     return result
 }
 
-//Proving the knowledge of exponent 
-const encryptedLmid = getInterSubPoly(encryptedLmids)
-const encryptedRmid = getInterSubPoly(encryptedRmids)
-const encryptedOmid = getInterSubPoly(encryptedOmids)
-const encryptedAlphaLmid = getInterSubPoly(encryptedAplhaLmids)
-const encryptedAlphaRmid = getInterSubPoly(encryptedAlphaLmids)
-const encryptedAlphaOmid = getInterSubPoly(encryptedAlphaLmids)
-const encryptedInterCombined = getInterSubPoly(encryptedSubPolynomialSum)
+// //Proving the knowledge of exponent 
+const encryptedLmid = getEncryptedInterSubPoly(encryptedLs);
+const encryptedRmid = getEncryptedInterSubPoly(encryptedRs);
+const encryptedOmid = getEncryptedInterSubPoly(encryptedOs);
+const encryptedAlphaLmid = getEncryptedInterSubPoly(encryptedAlphaLs);
+const encryptedAlphaRmid = getEncryptedInterSubPoly(encryptedAlphaRs);
+const encryptedAlphaOmid = getEncryptedInterSubPoly(encryptedAlphaOs);
+const encryptedInterCombined = getEncryptedInterSubPoly(encryptedSubPolynomialSum);
 
-+
+// function getEncryptedH(){
+//     for(let i =0; i<polynomialH.)
+// }
+
 
